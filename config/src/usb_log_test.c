28610351bae32e01f96c13da423bd28970064485
Brendon Dao <duybaodao@gmail.com>
2025-12-09 00:45:44 +0700

    fx

--- config/src/usb_log_test.c
+++ config/src/usb_log_test.c
@@ -1,11 +1,14 @@
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 // Work item that will run periodically
 static struct k_work_delayable log_work;
 
 // Function called each time the work runs
 static void log_work_handler(struct k_work *work) {
-    printk(">>> usb_log_test.c: Hello every 3 seconds! <<<\n");
+    LOG_INF(">>> usb_log_test.c: Hello every 3 seconds! <<<\n");
 
     // Reschedule to run again after 3000 ms
     k_work_reschedule(&log_work, K_MSEC(3000));
@@ -19,9 +22,13 @@ static int usb_log_test_init(void) {
     // Schedule first run after 3 seconds
     k_work_schedule(&log_work, K_MSEC(3000));
 
-    printk(">>> usb_log_test.c: SYS_INIT started, periodic logging enabled <<<\n");
+    LOG_INF(">>> usb_log_test.c: SYS_INIT started, periodic logging enabled <<<\n");
     return 0;
 }
 
 // Register init function to run at application startup
 SYS_INIT(usb_log_test_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
+adffddsafdfsfdadsfdfsfdsaaaaaaaaaaaaaaaa
+afdsfddfss
+adsfdsaf
